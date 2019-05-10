de31abb8228929a63d0c67689a2d9ea25d4fa0e6
Samuli Seppänen <samuli@openvpn.net>
2019-05-10 13:48:07 +0300

    Add support for Windows firewall exception on server peers
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -6,16 +6,21 @@
 # @param static_key
 #   The OpenVPN static key to use.
 # @param remote
-#   The IPv4 address of the peer. This should be an internal, non-VPN IP using which the remote can
-#   reached on UDP port 1194. On the "server" node you should omit this.
+#   The IPv4 address of the peer. This should be an internal, non-VPN IP using
+#   which the remote can reached. On the "server" node you should omit this.
+# @param port
+#   OpenVPN server port.
+# @param allow_address_ipv4
+#   The IPv4 address or network (e.g. "172.16.5.8") from which to allow
+#   connections to the "server". On the "clients" this has no effect.
 #
 define hlk_tap6_openvpn::instance
 (
   Stdlib::IP::Address::V4           $vpn_ip,
   String                            $static_key,
-  Integer                           $remote_port = 1194,
-  Optional[Stdlib::IP::Address::V4] $remote = undef
-
+  Integer                           $port = 1194,
+  Optional[Stdlib::IP::Address::V4] $remote = undef,
+  Optional[Stdlib::IP::Address::V4] $allow_address_ipv4 = '127.0.0.1'
 )
 {
 
@@ -33,11 +38,24 @@ define hlk_tap6_openvpn::instance
 
   if $remote {
     # This is a "client" instance and needs to know where to connect
-    $connectivity_line = "remote ${remote} ${remote_port} udp"
+    $connectivity_line = "remote ${remote} ${port} udp"
   } else {
     # This is a "server" instance and needs a port number for cases where
     # multiple OpenVPN instances are running on the same server.
-    $connectivity_line = "port ${remote_port}"
+    $connectivity_line = "port ${port}"
+
+    # The port also needs to be open or "clients" can't reach it
+    ::windows_firewall::exception { "HLK-OpenVPN-${identifier}-in":
+      ensure       => 'present',
+      direction    => 'in',
+      action       => 'allow',
+      enabled      => true,
+      protocol     => 'UDP',
+      local_port   => $port,
+      remote_ip    => $allow_address_ipv4,
+      display_name => "HLK-OpenVPN-${identifier}-in",
+      description  => "Allow HLK p2p OpenVPN connections to udp port ${port}",
+    }
   }
 
   # Install OpenVPN config
