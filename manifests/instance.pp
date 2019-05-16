8e68e423e87cf61b034e47f3b8339d87bee2c216
Samuli Seppänen <samuli@openvpn.net>
2019-05-16 16:54:52 +0300

    Add support for configuring the TAP adapter interface
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -9,6 +9,13 @@
 #   The OpenVPN static key to use.
 # @param port
 #   OpenVPN server port.
+# @interface_alias
+#   The alias (name) for the tap-windows6 adapter. For the support machine this
+#   should usually be set to 'SupportDevice0'.
+# @interface_ipv4
+#   The IPv4 address for the tap-windows6 adapter, including netmask in CIDR notation.
+# @interface_ipv6
+#   The IPv6 address for the tap-windows6 adapter, including netmask in CIDR notation.
 # @param allow_address_ipv4
 #   The IPv4 address or network (e.g. "172.16.5.8") from which to allow
 #   connections to the remote.
@@ -16,6 +23,9 @@
 define hlk_tap6_openvpn::instance
 (
   Stdlib::IP::Address::V4           $remote,
+  String                            $interface_alias,
+  Stdlib::IP::Address::V4           $interface_ipv4,
+  Stdlib::IP::Address::V6           $interface_ipv6,
   String                            $static_key,
   Integer                           $port = 1194,
   Optional[Stdlib::IP::Address::V4] $allow_address_ipv4 = '127.0.0.1'
@@ -51,5 +61,36 @@ define hlk_tap6_openvpn::instance
     content => template('hlk_tap6_openvpn/hlk.ovpn.erb'),
     require => Package['openvpn'],
     notify  => Service['openvpnservice'],
+  }
+
+  ### Configure the tap-windows6 adapter to make HLK tests go smoother
+  #
+  # Naming the tap-windows6 adapter as "SupportDevice0" ensures that HLK
+  # controller does not try to use the support machine for running HLK tests,
+  # except in the support role. This is needed because the support machine
+  # _has_ to be in the machine pool.
+  #
+  # This renaming may fail if there are multiple tap adapters on the system.
+  #
+  # See https://github.com/PowerShell/NetworkingDsc/wiki/NetAdapterName
+  dsc_xnetadaptername { $interface_alias:
+    dsc_newname              => $interface_alias,
+    dsc_interfacedescription => 'TAP-Windows Adapter V9',
+  }
+
+  # Define static IPv4 and IPv6 settings for the tap-windows6 adapter. This
+  # reduces the delay when bringing up and down the connection and makes it
+  # more likely that some of the more aggressively-timed HLK tests pass.
+  $tap_adapter_ips = {  'IPv4' => $interface_ipv4,
+                        'IPv6' => $interface_ipv6, }
+
+  # See https://github.com/PowerShell/NetworkingDsc/wiki/IPAddress
+  #
+  $tap_adapter_ips.each |$item| {
+    dsc_xipaddress { "${interface_alias}-${item[0]}":
+      dsc_interfacealias => $interface_alias,
+      dsc_ipaddress      => $item[1],
+      dsc_addressfamily  => $item[0],
+    }
   }
 }
